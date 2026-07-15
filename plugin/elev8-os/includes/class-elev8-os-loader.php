75987163e31fc654f0a4515d0f485c058d7c9e8a
stevekeln <44118741+stevekeln@users.noreply.github.com>
2026-07-14 18:01:22 -0600

    Add Business Intelligence Dashboard v1

--- plugin/elev8-os/includes/class-elev8-os-loader.php
+++ plugin/elev8-os/includes/class-elev8-os-loader.php
@@ -11,11 +11,14 @@ final class Elev8_OS_Loader {
         require_once ELEV8_OS_DIR . 'includes/Modules/class-elev8-os-dashboard-module.php';
         require_once ELEV8_OS_DIR . 'includes/Modules/class-elev8-os-system-inspector-module.php';
         require_once ELEV8_OS_DIR . 'includes/Modules/class-elev8-os-employee-mapping-module.php';
+        require_once ELEV8_OS_DIR . 'includes/Services/class-elev8-os-business-intelligence.php';
+        require_once ELEV8_OS_DIR . 'includes/Modules/class-elev8-os-business-intelligence-dashboard-module.php';
         require_once ELEV8_OS_DIR . 'includes/class-elev8-os.php';
         Elev8_OS::init();
         Elev8_OS_Artist_Portal_Module::init();
         Elev8_OS_Dashboard_Module::init();
         Elev8_OS_System_Inspector_Module::init();
         Elev8_OS_Employee_Mapping_Module::init();
+        Elev8_OS_Business_Intelligence_Dashboard_Module::init();
     }
 }
